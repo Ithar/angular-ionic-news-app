31254f80e44c045745ee416f64d71e8f8ddd0382
ithar.malik <ithar.malik@bjss.com>
2020-09-14 09:49:20 +0100

    Added publishers page

---
+++ onews/src/app/constants/publishers.ts
@@ -0,0 +1,518 @@
+export const PUBLISHERS = [
+  {
+    id: "1",
+    name: "ABC News",
+    logo: "assets/imgs/publishers/abcnews_logo.png",
+    code: 'abc-news'
+  },
+  {
+    id: "2",
+    name: "ABC News (AU)",
+    logo: "assets/imgs/publishers/abcnewsau_logo.png",
+    code: 'abc-news-au'
+  },
+  {
+    id: "3",
+    name: "Al Jazeera",
+    logo: "assets/imgs/publishers/aljazeera_logo.png",
+    code: 'al-jazeera-english'
+  },
+  {
+    id: "4",
+    name: "Ars Technica",
+    logo: "assets/imgs/publishers/arstechnica_logo.png",
+    code: 'ars-technica'
+  },
+  {
+    id: "5",
+    name: "Associated Press",
+    logo: "assets/imgs/publishers/associatedpress_logo.png",
+    code: 'associated-press'
+  },
+  {
+    id: "6",
+    name: "Australian Financial Review",
+    logo: "assets/imgs/publishers/australianfinancialreview_logo.png",
+    code: 'australian-financial-review'
+  },
+  {
+    id: "7",
+    name: "Axios",
+    logo: "assets/imgs/publishers/axios_logo.png",
+    code: 'axios'
+  },
+  {
+    id: "8",
+    name: "BBC News",
+    logo: "assets/imgs/publishers/bbcsports_logo.png",
+    code: 'bbc-news'
+  },
+  {
+    id: "9",
+    name: "BBC Sports",
+    logo: "assets/imgs/publishers/bbcsports_logo.png",
+    code: 'bbc-sport'
+  },
+  {
+    id: "10",
+    name: "Bleacher Report",
+    logo: "assets/imgs/publishers/bleacherreport_logo.png",
+    code: 'bleacher-report'
+  },
+  {
+    id: "11",
+    name: "Bloomberg",
+    logo: "assets/imgs/publishers/bloomberg_logo.png",
+    code: 'bloomberg'
+  },
+  {
+    id: "12",
+    name: "Breitbart News",
+    logo: "assets/imgs/publishers/breitbartnews_logo.png",
+    code: 'breitbart-news'
+  },
+  {
+    id: "13",
+    name: "Business Insider",
+    logo: "assets/imgs/publishers/businessinsider_logo.png",
+    code: 'business-insider'
+  },
+  {
+    id: "14",
+    name: "Business Insider (UK)",
+    logo: "assets/imgs/publishers/businessinsider_logo.png",
+    code: 'business-insider-uk'
+  },
+  {
+    id: "15",
+    name: "BuzzFeed",
+    logo: "assets/imgs/publishers/buzzfeednews_logo.png",
+    code: 'buzzfeed'
+  },
+  {
+    id: "16",
+    name: "CBC News",
+    logo: "assets/imgs/publishers/cbcnews_logo.png",
+    code: 'cbc-news'
+  },
+  {
+    id: "17",
+    name: "CNBC",
+    logo: "assets/imgs/publishers/cnbc_logo.png",
+    code: 'cnbc'
+  },
+  {
+    id: "18",
+    name: "CNN",
+    logo: "assets/imgs/publishers/cnn_logo.png",
+    code: 'cnn'
+  },
+  {
+    id: "19",
+    name: "Crypto Coins News",
+    logo: "assets/imgs/publishers/cryptocoinnews.png",
+    code: 'crypto-coins-news'
+  },
+  {
+    id: "20",
+    name: "Daily Mail",
+    logo: "assets/imgs/publishers/dialymail_logo.png",
+    code: 'daily-mail'
+  },
+  {
+    id: "21",
+    name: "Engadget",
+    logo: "assets/imgs/publishers/engadget_logo.png",
+    code: 'engadget'
+  },
+  {
+    id: "22",
+    name: "Entertainment Weekly",
+    logo: "assets/imgs/publishers/entertainmentweekly_logo.png",
+    code: 'entertainment-weekly'
+  },
+  {
+    id: "23",
+    name: "ESPN",
+    logo: "assets/imgs/publishers/espn_logo.png",
+    code: 'espn'
+  },
+  {
+    id: "24",
+    name: "ESPN Cric Info",
+    logo: "assets/imgs/publishers/espncricinfo_logo.png",
+    code: 'espn-cric-info'
+  },
+  {
+    id: "25",
+    name: "Financial Post",
+    logo: "assets/imgs/publishers/financialpost_logo.png",
+    code: 'financial-post'
+  },
+  {
+    id: "26",
+    name: "Financial Times",
+    logo: "assets/imgs/publishers/financialtimes_logo.png",
+    code: 'financial-times'
+  },
+  {
+    id: "27",
+    name: "Football Italia",
+    logo: "assets/imgs/publishers/footballitalia_logo.png",
+    code: 'football-italia'
+  },
+  {
+    id: "28",
+    name: "Fortune",
+    logo: "assets/imgs/publishers/fortune_logo.png",
+    code: 'fortune'
+  },
+  {
+    id: "29",
+    name: "FourFourTwo",
+    logo: "assets/imgs/publishers/fourfourtwo_logo.png",
+    code: 'four-four-two'
+  },
+  {
+    id: "30",
+    name: "Fox News",
+    logo: "assets/imgs/publishers/foxnews_logo.png",
+    code: 'fox-news'
+  },
+  {
+    id: "31",
+    name: "Fox Sports",
+    logo: "assets/imgs/publishers/foxsports_logo.png",
+    code: 'fox-sports'
+  },
+  {
+    id: "32",
+    name: "Google News",
+    logo: "assets/imgs/publishers/googlenews_logo.png",
+    code: 'google-news'
+  },
+  {
+    id: "33",
+    name: "Google News (Australia)",
+    logo: "assets/imgs/publishers/googlenews_logo.png",
+    code: 'google-news-au'
+  },
+  {
+    id: "34",
+    name: "Google News (Canada)",
+    logo: "assets/imgs/publishers/googlenews_logo.png",
+    code: 'google-news-ca'
+  },
+  {
+    id: "35",
+    name: "Google News (India)",
+    logo: "assets/imgs/publishers/googlenews_logo.png",
+    code: 'google-news-in'
+  },
+  {
+    id: "36",
+    name: "Google News (UK)",
+    logo: "assets/imgs/publishers/googlenews_logo.png",
+    code: 'google-news-uk'
+  },
+  {
+    id: "37",
+    name: "Hacker News",
+    logo: "assets/imgs/publishers/hackernews_logo.png",
+    code: 'hacker-news'
+  },
+  {
+    id: "38",
+    name: "IGN",
+    logo: "assets/imgs/publishers/ignnews_logo.png",
+    code: 'ign'
+  },
+  {
+    id: "39",
+    name: "Independent",
+    logo: "assets/imgs/publishers/independent_logo.png",
+    code: 'independent'
+  },
+  {
+    id: "40",
+    name: "Mashable",
+    logo: "assets/imgs/publishers/mashable_logo.png",
+    code: 'mashable'
+  },
+  {
+    id: "41",
+    name: "Medical News Today",
+    logo: "assets/imgs/publishers/medicalnewstoday_logo.png",
+    code: 'medical-news-today'
+  },
+  {
+    id: "42",
+    name: "Metro",
+    logo: "assets/imgs/publishers/metro_logo.png",
+    code: 'metro'
+  },
+  {
+    id: "43",
+    name: "Mirror",
+    logo: "assets/imgs/publishers/mirror_logo.png",
+    code: 'mirror'
+  },
+  {
+    id: "44",
+    name: "MSNBC",
+    logo: "assets/imgs/publishers/msnbc_logo.png",
+    code: 'msnbc'
+  },
+  {
+    id: "45",
+    name: "MTV News",
+    logo: "assets/imgs/publishers/mtvnews_logo.png",
+    code: 'mtv-news'
+  },
+  {
+    id: "46",
+    name: "MTV News (UK)",
+    logo: "assets/imgs/publishers/mtvnewsuk_logo.png",
+    code: 'mtv-news-uk'
+  },
+  {
+    id: "47",
+    name: "National Geographic",
+    logo: "assets/imgs/publishers/nationalgeographic_logo.png",
+    code: 'national-geographic'
+  },
+  {
+    id: "48",
+    name: "NBC News",
+    logo: "assets/imgs/publishers/nbcnews_logo.png",
+    code: 'nbc-news'
+  },
+  {
+    id: "49",
+    name: "News 24",
+    logo: "assets/imgs/publishers/news24_logo.png",
+    code: 'news24'
+  },
+  {
+    id: "50",
+    name: "New Scientist",
+    logo: "assets/imgs/publishers/newscientist_logo.png",
+    code: 'new-scientist'
+  },
+  {
+    id: "51",
+    name: "News.com.au",
+    logo: "assets/imgs/publishers/newscomau_logo.png",
+    code: 'news-com-au'
+  },
+  {
+    id: "52",
+    name: "Newsweek",
+    logo: "assets/imgs/publishers/newsweek_logo.png",
+    code: 'newsweek'
+  },
+  {
+    id: "53",
+    name: "New York Magazine",
+    logo: "assets/imgs/publishers/newyorkmagazine_logo.png",
+    code: 'new-york-magazine'
+  },
+  {
+    id: "54",
+    name: "Next Big Future",
+    logo: "assets/imgs/publishers/nextbigfuture_logo.png",
+    code: 'next-big-future'
+  },
+  {
+    id: "55",
+    name: "NFL News",
+    logo: "assets/imgs/publishers/nflnews_logo.png",
+    code: 'nfl-news'
+  },
+  {
+    id: "56",
+    name: "NHL News",
+    logo: "assets/imgs/publishers/nhlnews_logo.png",
+    code: 'nhl-news'
+  },
+  {
+    id: "57",
+    name: "Politico",
+    logo: "assets/imgs/publishers/polictico_logo.png",
+    code: 'politico'
+  },
+  {
+    id: "58",
+    name: "Polygon",
+    logo: "assets/imgs/publishers/polygon_logo.png",
+    code: 'polygon'
+  },
+  {
+    id: "59",
+    name: "Recode",
+    logo: "assets/imgs/publishers/recode_logo.png",
+    code: 'recode'
+  },
+  {
+    id: "60",
+    name: "Reddit/r/all",
+    logo: "assets/imgs/publishers/reddit_logo.png",
+    code: 'reddit-r-all'
+  },
+  {
+    id: "61",
+    name: "Reuters",
+    logo: "assets/imgs/publishers/reuters_logo.png",
+    code: 'reuters'
+  },
+  {
+    id: "62",
+    name: "RTE",
+    logo: "assets/imgs/publishers/rte_logo.png",
+    code: 'rte'
+  },
+  {
+    id: "63",
+    name: "TalkSport",
+    logo: "assets/imgs/publishers/talksport_logo.png",
+    code: 'talksport'
+  },
+  {
+    id: "64",
+    name: "TechCrunch",
+    logo: "assets/imgs/publishers/techcrunch_logo.png",
+    code: 'techcrunch'
+  },
+  {
+    id: "65",
+    name: "TechRadar",
+    logo: "assets/imgs/publishers/techradar_logo.png",
+    code: 'techradar'
+  },
+  {
+    id: "66",
+    name: "The Economist",
+    logo: "assets/imgs/publishers/theeconomist_logo.png",
+    code: 'the-economist'
+  },
+  {
+    id: "67",
+    name: "The Global And Mail",
+    logo: "assets/imgs/publishers/theglobeandmail_logo.png",
+    code: 'the-globe-and-mail'
+  },
+  {
+    id: "68",
+    name: "The Guardian (AU)",
+    logo: "assets/imgs/publishers/theguardian_logo.png",
+    code: 'the-guardian-au'
+  },
+  {
+    id: "69",
+    name: "The Guardian (UK)",
+    logo: "assets/imgs/publishers/theguardian_logo.png",
+    code: 'the-guardian-uk'
+  },
+  {
+    id: "71",
+    name: "The Hill",
+    logo: "assets/imgs/publishers/thehill_logo.png",
+    code: 'the-hill'
+  },
+  {
+    id: "72",
+    name: "The Hindu",
+    logo: "assets/imgs/publishers/thehindu_logo.png",
+    code: 'the-hindu'
+  },
+  {
+    id: "73",
+    name: "The Huffington Post",
+    logo: "assets/imgs/publishers/thehuffingtonpost_logo.png",
+    code: 'the-huffington-post'
+  },
+  {
+    id: "74",
+    name: "The Irish Times",
+    logo: "assets/imgs/publishers/theirishtimes_logo.png",
+    code: 'the-irish-times'
+  },
+  {
+    id: "75",
+    name: "The Lad Bible",
+    logo: "assets/imgs/publishers/theladbible_logo.png",
+    code: 'the-lad-bible'
+  },
+  {
+    id: "76",
+    name: "The New York Times",
+    logo: "assets/imgs/publishers/newyorktimes_logo.png",
+    code: 'the-new-york-times'
+  },
+  {
+    id: "77",
+    name: "The Next Web",
+    logo: "assets/imgs/publishers/thenextweb_logo.png",
+    code: 'the-next-web'
+  },
+  {
+    id: "78",
+    name: "The Sport Bible",
+    logo: "assets/imgs/publishers/thesportsbible_logo.png",
+    code: 'the-sport-bible'
+  },
+  {
+    id: "79",
+    name: "The Telegraph",
+    logo: "assets/imgs/publishers/thetelegraph_logo.png",
+    code: 'the-telegraph'
+  },
+  {
+    id: "80",
+    name: "The Times Of India",
+    logo: "assets/imgs/publishers/thetimesofindia.png",
+    code: 'the-times-of-india'
+  },
+  {
+    id: "81",
+    name: "The Verge",
+    logo: "assets/imgs/publishers/verge_logo.png",
+    code: 'the-verge'
+  },
+  {
+    id: "82",
+    name: "The Wall Street Journal",
+    logo: "assets/imgs/publishers/wallstreetjournal_logo.png",
+    code: 'the-wall-street-journal'
+  },
+  {
+    id: "83",
+    name: "The Washington Post",
+    logo: "assets/imgs/publishers/washingtonpost_logo.png",
+    code: 'the-washington-post'
+  },
+  {
+    id: "84",
+    name: "Time",
+    logo: "assets/imgs/publishers/time_logo.png",
+    code: 'time'
+  },
+  {
+    id: "85",
+    name: "USA Today",
+    logo: "assets/imgs/publishers/usatoday_logo.png",
+    code: 'usa-today'
+  },
+  {
+    id: "86",
+    name: "Vice News",
+    logo: "assets/imgs/publishers/vicenews_logo.png",
+    code: 'vice-news'
+  },
+  {
+    id: "87",
+    name: "Wired",
+    logo: "assets/imgs/publishers/wired_logo.png",
+    code: 'wired'
+  }
+]
--- onews/src/app/publishers/publishers.page.ts
+++ onews/src/app/publishers/publishers.page.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PUBLISHERS } from '../constants/publishers';
+
 @Component({
   selector: 'app-publishers',
   templateUrl: './publishers.page.html',
   styleUrls: ['./publishers.page.scss'],
 })
 export class PublishersPage implements OnInit {
+
+  private publishers: Array<any> = PUBLISHERS;
 
   constructor() { }
 
