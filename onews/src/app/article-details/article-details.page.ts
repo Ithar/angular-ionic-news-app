2f53bc5b0a044a2b0a84c15f3a692db32863b7e6
ithar.malik <ithar.malik@bjss.com>
2020-09-14 15:11:31 +0100

    Add/Remove from favorites

--- onews/src/app/article-details/article-details.page.ts
+++ onews/src/app/article-details/article-details.page.ts
@@ -1,6 +1,6 @@
+import { ToastMessageService } from './../service/toast-message.service';
 import { Component, OnInit } from '@angular/core';
 import { Storage } from '@ionic/storage';
-
 
 @Component({
   selector: 'app-article-details',
@@ -9,9 +9,10 @@ import { Storage } from '@ionic/storage';
 })
 export class ArticleDetailsPage implements OnInit {
 
-  article: any = {};
+  private article: any = {};
+  private isFavoriate = false;
 
-  constructor(private storage: Storage) { }
+  constructor(private storage: Storage, private toastMessageService: ToastMessageService) { }
 
   ngOnInit() {
     this.getArticle();
@@ -23,4 +24,32 @@ export class ArticleDetailsPage implements OnInit {
       this.article = article;
     } 
   }
+
+  private async addToFavoriates(article: any) {
+    const favoriates = await this.storage.get('favoriates');
+    if (favoriates != null) {
+      favoriates.push(article);
+      this.storage.set('favoriates', favoriates);
+    } else {
+      this.storage.set('favoriates', [article]);
+    }
+
+    this.toastMessageService.presentToast('Added to favoriates');
+    this.isFavoriate = true;
+  }
+
+
+  private async removeFromFavoriates(article: any) {
+    
+    const url = article.url;
+
+    const favoriates = await this.storage.get('favoriates');
+
+    if (favoriates != null) {
+      this.storage.set('favoriates' , favoriates.filter(article => article.url !== url));
+    }
+
+    this.isFavoriate = false;
+  }
+
 }
